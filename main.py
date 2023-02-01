321bae2be92d786897f2c0dbe60a1f46130dc74d
ilyas424 <ilyasich123@gmail.com>
2023-02-01 12:30:01 +0300

    sqlalchemy

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+import psycopg2
+import psycopg2.extras
 
 app = FastAPI()
 
 
+conn = psycopg2.connect(
+ database="JIRA",
+ user="postgres",
+ password="ilyas13!A",
+ host="127.0.0.1",
+ port="5432")
+
+
 @app.get("/")
 def home():
-    return {"messag": "Hello World yes"}
+    return {"sms": "Hello World yes"}
+
+
+
