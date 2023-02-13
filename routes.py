abefcf85f7fc45f8783d15b9230f3e4fe08e8c48
ilyas424 <ilyasich123@gmail.com>
2023-02-14 01:39:48 +0300

    add response 404

--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
+from fastapi import status
 from starlette.requests import Request
 from schema import CommentText
 from schema import  TicketCreate
-
+from fastapi.responses import JSONResponse
 import utils
 
 
@@ -39,7 +40,6 @@ def get_ticket_all(request: Request):
     tickets = utils.get_tickets_from_db(session)
     for ticket in tickets:
         result.append(utils.convert_ticket_object_to_json(ticket))
-    # result = [{"description": val[0], "creation_date": val[1], "time_estimate": val[2],"priority": val[3], "type": val[4], "status": val[5], "reporter": val[6], "assigneee": val[7] } for val in obj]
     return result
 
 @router.post('/ticket')
@@ -55,7 +55,13 @@ def get_ticket_by_id(request: Request, id: int):
     result = {}
     session = request.state.db
     ticket = utils.get_ticket_from_db(session, id)
-    result = utils.convert_ticket_object_to_json(ticket)
+    if ticket == None:  
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Тикет не найден" }
+        )
+    else:
+        result = utils.convert_ticket_object_to_json(ticket)
     return result
 
 
@@ -72,6 +78,11 @@ def get_comments_by_ticket_id(request: Request, id: int):
     result = {}
     session = request.state.db
     result = utils.get_comments_by_ticket_id_from_db(session, id)
+    if result == []:  
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Тикет не найден" }
+        )
     return result
 
 @router.get('/ticket/{id}/comment/{comment_id}')
@@ -79,6 +90,11 @@ def get_comment_by_ticket_id(request: Request, id: int, comment_id: int):
     result = {}
     session = request.state.db
     result = utils.get_comment_by_ticket_id_from_db(session, id, comment_id)
+    if result == []:  
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "коментарий не найден" }
+        )
     return result
 
 @router.delete('/ticket/{id}/comment/{comment_id}')
@@ -109,4 +125,9 @@ def get_user_by_id(request: Request, id: int):
     result = {}
     session = request.state.db
     result = utils.get_user_from_db(session, id)
+    if result == []:  
+        return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Пользователь  не найден" }
+        )
     return result
